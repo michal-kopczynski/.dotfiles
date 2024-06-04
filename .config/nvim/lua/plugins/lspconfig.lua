abcb3801e81bfd762ad5e9325155e03d9ebc73ff
Michal Kopczynski <26494112+michal-kopczynski@users.noreply.github.com>
2024-06-04 23:17:37 +0200

    Add go LSP

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -157,7 +157,8 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        templ = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
