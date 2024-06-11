bfc29533f78764cbd1bf5723ce7c8ea764a48deb
Michal Kopczynski <26494112+michal-kopczynski@users.noreply.github.com>
2024-06-11 15:18:12 +0200

    Disable vim swapfile

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -64,3 +64,6 @@ vim.opt.scrolloff = 10
 -- (for obsidian) Concealing characters
 -- If you wish to use the formatting concealment features, you will need to have conceallevel set to a value that allows it (either 1 or 2), for example: set conceallevel=1 in viml or vim.opt.conceallevel = 1 in a lua config.
 vim.opt.conceallevel = 1
+
+-- disable swapfile
+vim.opt.swapfile = false
